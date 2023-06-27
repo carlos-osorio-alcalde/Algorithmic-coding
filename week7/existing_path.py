f73364004130bc6cf2623c62645a309c757eee55
carlos-osorio-alcalde <carlos.osorio@factored.ai>
2023-06-27 09:56:59 -0500

    week7 solutions

--- week7/existing_path.py
+++ week7/existing_path.py
@@ -1,1 +1,33 @@
 """https://leetcode.com/problems/find-if-path-exists-in-graph/"""
+
+from typing import List
+
+
+class Solution:
+    def validPath(
+        self, n: int, edges: List[List[int]], source: int, destination: int
+    ) -> bool:
+        neighbors_for_each_node = {}
+        for edge in edges:
+            for i in range(2):
+                if edge[i] in neighbors_for_each_node:
+                    neighbors_for_each_node[edge[i]].append(edge[1 - i])
+                else:
+                    neighbors_for_each_node[edge[i]] = [edge[1 - i]]
+
+        visited_nodes = set()
+
+        def search(source):
+            visited_nodes.add(source)
+            if source == destination:
+                return True
+
+            neighbors = neighbors_for_each_node[source]
+            for node in neighbors:
+                if node not in visited_nodes:
+                    if search(node):
+                        return True
+
+            return False
+
+        return search(source)
--- week7/subarray_product.py
+++ week7/subarray_product.py
@@ -1,1 +1,41 @@
 """https://leetcode.com/problems/subarray-product-less-than-k/"""
+
+# First attempt: Brute force (Time limit exceeded :( )
+from typing import List
+
+
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        n_subarrays = 0
+        n = len(nums)
+        
+        for i in range(n):
+            product = 1
+            for j in range(i, n):
+                product *= nums[j]
+                if product < k:
+                    n_subarrays += 1
+        
+        return n_subarrays
+
+
+# Second attempt: Two pointers - like solution
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        if k <= 1:
+            return 0
+        
+        n_subarrays = 0
+        prod = 1
+
+        i = 0
+        j = 0
+
+        for j, num in enumerate(nums):
+            prod *= num
+            while (prod >= k) and (i <= j):
+                prod /= nums[i]
+                i += 1
+            n_subarrays += 1 + (j - i)
+            
+        return n_subarrays
